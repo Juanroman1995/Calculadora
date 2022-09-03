aa25163594f6769faa4aa386397852fb98f0444e
Juanroman1995 <109772447+Juanroman1995@users.noreply.github.com>
2022-09-03 02:14:01 -0300

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -1,1 +1,24 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
 
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+    
+    raizcuadrada(num1,num2){
+        return Math.pow(num2,1/num1)
+    }
+    potencia(num1,num2){
+        return Math.pow(num1,num2);
+    }
+} 
